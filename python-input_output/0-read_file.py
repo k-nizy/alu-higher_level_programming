e8135c91ddbf7f08ad4a21f4cdc589e8be1892ce
k-nizy <k.nizeyiman@gmail.com>
2025-03-11 07:55:24 -0700

    added the my_file_0.txt

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
+"""
+This module provides a function to read and print the contents of a text file.
+
+The function `read_file` reads a file specified by the filename and prints its
+contents to standard output. It uses the `with` statement to ensure proper file
+handling and does not require any external modules.
+
+Example:
+    To use this module, import it and call the `read_file` function with the
+    filename as an argument.
+
+    ```python
+    read_file = __import__('0-read_file').read_file
+    read_file("my_file_0.txt")
+    ```
+"""
+
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints its contents to stdout.
