b320f2b3e5b1213ff44bffa327f8b314beaee1ec
k-nizy <k.nizeyiman@gmail.com>
2025-03-25 07:47:47 -0700

    added the 0- add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -18,9 +18,9 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If a or b is not an integer or float.
     """
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)) or isinstance(a, float) and a != a:  # Check for NaN
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)) or isinstance(b, float) and b != b:  # Check for NaN
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
