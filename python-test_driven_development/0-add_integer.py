50facf11b634dc2fa79b7c87cb68a35acf62ad7c
k-nizy <k.nizeyiman@gmail.com>
2025-03-25 08:07:35 -0700

    added thetext

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -18,9 +18,9 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If a or b is not an integer or float.
     """
-    if not isinstance(a, (int, float)) or isinstance(a, float) and a != a:  # Check for NaN
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)) or isinstance(b, float) and b != b:  # Check for NaN
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
