f2b9477832f77cf8d5bd1935a45a2bf77db07bd3
k-nizy <k.nizeyiman@gmail.com>
2025-03-27 05:19:13 -0700

    object relational mapping

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""
-This module provides a function to add two integers.
-"""
+"""Defines an  an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """
-    Adds two integers.
-
-    Args:
-        a (int or float): The first number.
-        b (int or float, optional): The second number. Defaults to 98.
-
-    Returns:
-        int: The sum of a and b.
-
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
+    Second integer has a default value of 98 if value is not specified
     Raises:
-        TypeError: If a or b is not an integer or float.
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance(a, (int, float)):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    return int(a) + int(b)
+    return (int(a) + int(b))
